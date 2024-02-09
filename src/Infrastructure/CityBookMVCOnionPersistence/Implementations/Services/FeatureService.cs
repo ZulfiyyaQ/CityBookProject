0ec89a1cf64ecf1085ffce6ad88705075dc2cb5b
ZulfiyyaQ <tu7u71zbq@code.edu.az>
2024-02-09 22:57:06 +0400

    added Features Service

--- src/Infrastructure/CityBookMVCOnionPersistence/Implementations/Services/FeatureService.cs
+++ src/Infrastructure/CityBookMVCOnionPersistence/Implementations/Services/FeatureService.cs
@@ -1,12 +1,234 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using AutoMapper;
+using CityBookMVCOnionApplication.Abstractions.Services;
+using CityBookMVCOnionApplication.ViewModels.Feature;
+using CityBookMVCOnionApplication.ViewModels;
+using CityBookMVCOnionInfrastructure.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq.Expressions;
+using CityBookMVCOnionApplication.Abstractions.Repositories;
+using CityBookMVCOnionDomain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CityBookMVCOnionPersistence.Implementations.Services
 {
-    internal class FeatureService
+    public class FeatureService : IFeatureService
     {
+        private readonly IMapper _mapper;
+        private readonly IFeatureRepository _repository;
+        private readonly IHttpContextAccessor _http;
+        private readonly UserManager<User> _userManager;
+
+        public FeatureService(IMapper mapper, IFeatureRepository repository,
+            IHttpContextAccessor http, UserManager<User> userManager)
+        {
+            _mapper = mapper;
+            _repository = repository;
+            _http = http;
+            _userManager = userManager;
+        }
+
+        public async Task<bool> CreateAsync(CreateFeatureVM create, ModelStateDictionary model)
+        {
+            if (!model.IsValid) return false;
+            if (await _repository.CheckUniqueAsync(x => x.Name.ToLower().Trim() == create.Name.ToLower().Trim()))
+            {
+                model.AddModelError("Name", "Name is exists");
+                return false;
+            }
+            Feature item = _mapper.Map<Feature>(create);
+            //item.CreatedBy = user.UserName;
+
+            await _repository.AddAsync(item);
+            await _repository.SaveChanceAsync();
+
+            return true;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            if (id <= 0) throw new WrongRequestException("The request sent does not exist");
+            Feature item = await _repository.GetByIdAsync(id);
+            if (item == null) throw new NotFoundException("Your request was not found");
+            _repository.Delete(item);
+            await _repository.SaveChanceAsync();
+        }
+
+        public async Task<ICollection<ItemFeatureVM>> GetAllWhereAsync(int take, int page = 1)
+        {
+            ICollection<Feature> items = await _repository
+                .GetAllWhere(skip: (page - 1) * take, take: take, IsTracking: false).ToListAsync();
+
+            ICollection<ItemFeatureVM> vMs = _mapper.Map<ICollection<ItemFeatureVM>>(items);
+            return vMs;
+        }
+
+        public async Task<ICollection<ItemFeatureVM>> GetAllWhereByOrderAsync(int take, Expression<Func<Feature, object>>? orderExpression, int page = 1)
+        {
+            ICollection<Feature> items = await _repository
+                    .GetAllWhereByOrder(orderException: orderExpression, skip: (page - 1) * take, take: take, IsTracking: false).ToListAsync();
+
+            ICollection<ItemFeatureVM> vMs = _mapper.Map<ICollection<ItemFeatureVM>>(items);
+
+            return vMs;
+        }
+        public async Task<PaginationVM<ItemFeatureVM>> GetFilteredAsync(string? search, int take, int page, int order)
+        {
+            if (page <= 0) throw new WrongRequestException("The request sent does not exist");
+            if (order <= 0) throw new WrongRequestException("The request sent does not exist");
+
+            string[] includes = { $"{nameof(Feature.PlaceFeature)}" };
+            double count = await _repository.CountAsync();
+
+            ICollection<Feature> items = new List<Feature>();
+
+            switch (order)
+            {
+                case 1:
+                    items = await _repository
+                    .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                        x => x.Name, false, false, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+                case 2:
+                    items = await _repository
+                     .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                      x => x.CreateAt, false, false, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+                case 3:
+                    items = await _repository
+                    .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                        x => x.Name, true, false, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+                case 4:
+                    items = await _repository
+                    .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                      x => x.CreateAt, true, false, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+            }
+
+            ICollection<ItemFeatureVM> vMs = _mapper.Map<ICollection<ItemFeatureVM>>(items);
+
+            PaginationVM<ItemFeatureVM> pagination = new PaginationVM<ItemFeatureVM>
+            {
+                Take = take,
+                Search = search,
+                Order = order,
+                CurrentPage = page,
+                TotalPage = Math.Ceiling(count / take),
+                Items = vMs
+            };
+
+            return pagination;
+        }
+        public async Task<PaginationVM<ItemFeatureVM>> GetDeleteFilteredAsync(string? search, int take, int page, int order)
+        {
+            if (page <= 0) throw new WrongRequestException("The request sent does not exist");
+            if (order <= 0) throw new WrongRequestException("The request sent does not exist");
+
+            string[] includes = { $"{nameof(Feature.PlaceFeature)}" };
+            double count = await _repository.CountAsync();
+
+            ICollection<Feature> items = new List<Feature>();
+
+            switch (order)
+            {
+                case 1:
+                    items = await _repository
+                    .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                        x => x.Name, false, true, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+                case 2:
+                    items = await _repository
+                     .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                     orderException: x => x.CreateAt, false, true, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+                case 3:
+                    items = await _repository
+                    .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                        x => x.Name, true, true, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+                case 4:
+                    items = await _repository
+                    .GetAllWhereByOrder(x => !string.IsNullOrEmpty(search) ? x.Name.ToLower().Contains(search.ToLower()) : true,
+                      x => x.CreateAt, true, true, (page - 1) * take, take, false, includes).ToListAsync();
+                    break;
+            }
+
+            ICollection<ItemFeatureVM> vMs = _mapper.Map<ICollection<ItemFeatureVM>>(items);
+
+            PaginationVM<ItemFeatureVM> pagination = new PaginationVM<ItemFeatureVM>
+            {
+                Take = take,
+                Search = search,
+                Order = order,
+                CurrentPage = page,
+                TotalPage = Math.Ceiling(count / take),
+                Items = vMs
+            };
+
+            return pagination;
+        }
+        public async Task<GetFeatureVM> GetByIdAsync(int id)
+        {
+            if (id <= 0) throw new WrongRequestException("The request sent does not exist");
+            string[] includes = { $"{nameof(Feature.PlaceFeature)}" };
+            Feature item = await _repository.GetByIdAsync(id, IsTracking: false, includes: includes);
+            if (item == null) throw new NotFoundException("Your request was not found");
+
+            GetFeatureVM get = _mapper.Map<GetFeatureVM>(item);
+
+            return get;
+        }
+
+        public async Task ReverseSoftDeleteAsync(int id)
+        {
+            if (id <= 0) throw new WrongRequestException("The request sent does not exist");
+            Feature item = await _repository.GetByIdAsync(id);
+            if (item == null) throw new NotFoundException("Your request was not found");
+
+            item.IsDeleted = false;
+            await _repository.SaveChanceAsync();
+        }
+
+        public async Task SoftDeleteAsync(int id)
+        {
+            if (id <= 0) throw new WrongRequestException("The request sent does not exist");
+            Feature item = await _repository.GetByIdAsync(id);
+            if (item == null) throw new NotFoundException("Your request was not found");
+
+            item.IsDeleted = true;
+            await _repository.SaveChanceAsync();
+        }
+
+        public async Task<UpdateFeatureVM> UpdateAsync(int id)
+        {
+            if (id <= 0) throw new WrongRequestException("The request sent does not exist");
+            Feature item = await _repository.GetByIdAsync(id);
+            if (item == null) throw new NotFoundException("Your request was not found");
+
+            UpdateFeatureVM update = _mapper.Map<UpdateFeatureVM>(item);
+
+            return update;
+        }
+
+        public async Task<bool> UpdatePostAsync(int id, UpdateFeatureVM update, ModelStateDictionary model)
+        {
+            if (id <= 0) throw new WrongRequestException("The request sent does not exist");
+            Feature item = await _repository.GetByIdAsync(id);
+            if (item == null) throw new NotFoundException("Your request was not found");
+
+            if (await _repository.CheckUniqueAsync(x => x.Name.ToLower().Trim() == update.Name.ToLower().Trim() && x.Id != id))
+            {
+                model.AddModelError("Name", "Name is exists");
+                return false;
+            }
+            _mapper.Map(update, item);
+            //item.CreatedBy = user.UserName;
+            _repository.Update(item);
+            await _repository.SaveChanceAsync();
+            return true;
+        }
     }
 }
